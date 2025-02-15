c9927c89a8200f2b113b8a4489072c38ed2a0d68
Flo Kleiser <floriankleiser@gmail.com>
2025-02-15 14:01:20 +0100

    Initial Commit

---
+++ TramDisplay/TramDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TramDisplayApp.swift
+//  TramDisplay
+//
+//  Created by Flo Kleiser on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct TramDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
