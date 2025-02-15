13ad78fc4ce8f2583a409396a2a0fe5cc304c392
Flo Kleiser <floriankleiser@gmail.com>
2025-02-15 16:30:51 +0100

    add readme

---
+++ TramDisplayWidget/TramDisplayWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TramDisplayWidgetBundle.swift
+//  TramDisplayWidget
+//
+//  Created by Flo Kleiser on 15.02.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TramDisplayWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TramDisplayWidget()
+    }
+}
