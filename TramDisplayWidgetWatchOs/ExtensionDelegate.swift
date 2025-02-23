babebafac9d62a1308fc73822b48edf6ae70e036
Flo Kleiser <floriankleiser@gmail.com>
2025-02-23 13:26:57 +0100

    more complication design

--- TramDisplayWidgetWatchOs/ExtensionDelegate.swift
+++ TramDisplayWidgetWatchOs/ExtensionDelegate.swift
@@ -4,7 +4,7 @@ import ClockKit
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
     func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
         for task in backgroundTasks {
-            if let complicationTask = task as? WKApplicationRefreshBackgroundTask {
+            if task is WKApplicationRefreshBackgroundTask {
                 let complicationServer = CLKComplicationServer.sharedInstance()
                 for complication in complicationServer.activeComplications ?? [] {
                     complicationServer.reloadTimeline(for: complication)
--- TramDisplayWidgetWatchOs/TramDisplayWidgetWatchOs.swift
+++ TramDisplayWidgetWatchOs/TramDisplayWidgetWatchOs.swift
@@ -6,29 +6,41 @@ struct ComplicationProvider: TimelineProvider {
     let transportService = TransportService()
     
     func getSnapshot(in context: Context, completion: @escaping (ComplicationEntry) -> Void) {
-        let entry = ComplicationEntry(date: Date(), departure: Departure(time: Date()))
+        let entry = ComplicationEntry(
+            date: Date(),
+//            departure: Departure(time: Date()))
+            departures: [Departure(time: Date())]
+            )
         completion(entry)
     }
     
     func getTimeline(in context: Context, completion: @escaping (Timeline<ComplicationEntry>) -> Void) {
         transportService.fetchDepartures(station: "Zürich, Toni-Areal", destination: "Zürich, Rathaus") { departures in
-            let entries = departures.map { departure in
-                ComplicationEntry(date: Date(), departure: departure)
-            }
+//            let entries = departures.map { departure in
+//                ComplicationEntry(date: Date(), departure: departure)
+//            }
+            let entry = ComplicationEntry(date: Date(), departures: departures)
             
-            let timeline = Timeline(entries: entries, policy: .after(Date().addingTimeInterval(15 * 60)))
+//            let timeline = Timeline(entries: entries, policy: .after(Date().addingTimeInterval(15 * 60)))
+//            completion(timeline)
+            let timeline = Timeline(entries: [entry], policy: .after(Date().addingTimeInterval(15*60)))
             completion(timeline)
         }
     }
     
     func placeholder(in context: Context) -> ComplicationEntry {
-        ComplicationEntry(date: Date(), departure: Departure(time: Date()))
+        ComplicationEntry(
+            date: Date(),
+//            departure: Departure(time: Date())
+            departures: [Departure(time: Date())]
+        )
     }
 }
 
 struct ComplicationEntry: TimelineEntry {
     let date: Date
-    let departure: Departure
+//    let departure: Departure
+    let departures: [Departure]
 }
 
 struct ComplicationView: View {
@@ -45,38 +57,38 @@ struct ComplicationView: View {
     
     var body: some View {
         switch family {
-        case .accessoryCircular:
-            AccessoryCircularView(entry: entry)
+//        case .accessoryCircular:
+//            AccessoryCircularView(entry: entry)
         case .accessoryCorner:
             AccessoryCornerView(entry: entry)
-        case .accessoryRectangular:
-            AccessoryRectangularView(entry: entry)
+//        case .accessoryRectangular:
+//            AccessoryRectangularView(entry: entry)
         default:
-            AccessoryCircularView(entry: entry)
+            AccessoryCornerView(entry: entry)
         }
     }
 }
 
-struct AccessoryCircularView: View {
-    let entry: ComplicationEntry
-    
-    private let timeFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        formatter.locale = Locale(identifier: "de_CH")
-        return formatter
-    }()
-    
-    var body: some View {
-        VStack {
-            Image(systemName: "tram.fill")
-            Text(timeFormatter.string(from: entry.departure.time))
-                .font(.caption2)
-        }
-        .containerBackground(Color(red: 24/255, green: 27/255, blue: 31/255), for: .widget)
-
-    }
-}
+//struct AccessoryCircularView: View {
+//    let entry: ComplicationEntry
+//    
+//    private let timeFormatter: DateFormatter = {
+//        let formatter = DateFormatter()
+//        formatter.timeStyle = .short
+//        formatter.locale = Locale(identifier: "de_CH")
+//        return formatter
+//    }()
+//    
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "tram.fill")
+//            Text(timeFormatter.string(from: entry.departure.time))
+//                .font(.caption2)
+//        }
+//        .containerBackground(Color(red: 24/255, green: 27/255, blue: 31/255), for: .widget)
+//
+//    }
+//}
 
 struct AccessoryCornerView: View {
     let entry: ComplicationEntry
@@ -87,62 +99,118 @@ struct AccessoryCornerView: View {
         formatter.locale = Locale(identifier: "de_CH")
         return formatter
     }()
+
+//    var body: some View {
+//        ZStack {
+//            AccessoryWidgetBackground()
+//            Image(systemName: "tram.fill")
+//                .imageScale(.small)
+//                .font(.caption2)
+//                .frame(width: 10, height: 10)
+//                .widgetCurvesContent()
+//            
+//                .widgetLabel {
+////                    Text(timeFormatter.string(from: entry.departure.time))
+////                    .font(.caption)
+//                    HStack(spacing: 2) {
+//                        Text(timeFormatter.string(from: entry.departure.time))
+//                        Text("•")
+//                        .foregroundStyle(.secondary)
+//                        Text(timeFormatter.string(from: entry.departure.time.addingTimeInterval(10 * 60)))
+//                        Text("•")
+//                        .foregroundStyle(.secondary)
+//                        Text(timeFormatter.string(from: entry.departure.time.addingTimeInterval(20 * 60)))
+//                    }
+//                    .font(.caption2)
+//                }
+////                .containerBackground(.white.gradient, for: .widget)
+//                  .containerBackground(.clear, for: .widget)
+//            
+//        }
+//    }
     
     var body: some View {
-        VStack {
-            Text("Next")
-                .font(.caption2)
-            Text(timeFormatter.string(from: entry.departure.time))
-                .font(.caption)
+        ZStack {
+//            Image(systemName: "tram.fill")
+            let times = entry.departures.prefix(1).map { timeFormatter.string(from: $0.time) }
+            Text(times.joined(separator: " • "))
+                    .font(.system(size: 12))
+
+
+                .widgetLabel {
+//                    Text("\(timeFormatter.string(from: entry.departure.time)) • \(timeFormatter.string(from: entry.departure.time.addingTimeInterval(10 * 60))) • \(timeFormatter.string(from: entry.departure.time.addingTimeInterval(20 * 60)))")
+                Text("Next Tram")
+                        .font(.system(size: 2))
+//                        .font(.caption)
+                 }
         }
-        .containerBackground(Color(red: 24/255, green: 27/255, blue: 31/255), for: .widget)
-
-    }
-    
-}
-
-struct AccessoryRectangularView: View {
-    let entry: ComplicationEntry
-    
-    private let timeFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        formatter.locale = Locale(identifier: "de_CH")
-        return formatter
-    }()
-    
-    var body: some View {
-        HStack {
-            Image(systemName: "tram.fill")
-            VStack(alignment: .leading) {
-                Text("Next departure")
-                    .font(.caption2)
-                Text(timeFormatter.string(from: entry.departure.time))
-                    .font(.caption)
-            }
+                .widgetCurvesContent()
+                .containerBackground(.clear, for: .widget)
         }
-        .containerBackground(Color(red: 24/255, green: 27/255, blue: 31/255), for: .widget)
 
-    }
 }
+    
+    
+// -kinda working
+//    var body: some View {
+//           Image(systemName: "tram.fill")
+//               .imageScale(.small)
+//               .font(.system(size: 12))
+//               .widgetCurvesContent()
+//               .widgetLabel {
+//                   let times = entry.departures.prefix(3).map { timeFormatter.string(from: $0.time) }
+//                   Text(times.joined(separator: " • "))
+//                       .font(.system(size: 12))
+//               }
+//               .containerBackground(.clear, for: .widget)
+//       }
+//   }
 
-#Preview(as: .accessoryCircular) {
-    TramComplication()
-} timeline: {
-    ComplicationEntry(date: Date(), departure: Departure(time: Date()))
-    ComplicationEntry(date: Date().addingTimeInterval(3600), departure: Departure(time: Date().addingTimeInterval(3600)))
-}
 
-#Preview(as: .accessoryRectangular) {
-    TramComplication()
-} timeline: {
-    ComplicationEntry(date: Date(), departure: Departure(time: Date()))
-}
+//struct AccessoryRectangularView: View {
+//    let entry: ComplicationEntry
+//    
+//    private let timeFormatter: DateFormatter = {
+//        let formatter = DateFormatter()
+//        formatter.timeStyle = .short
+//        formatter.locale = Locale(identifier: "de_CH")
+//        return formatter
+//    }()
+//    
+//    var body: some View {
+//        HStack {
+//            Image(systemName: "tram.fill")
+//            VStack(alignment: .leading) {
+//                Text("Next departure")
+//                    .font(.caption2)
+//                Text(timeFormatter.string(from: entry.departure.time))
+//                    .font(.caption)
+//            }
+//        }
+//        .containerBackground(Color(red: 24/255, green: 27/255, blue: 31/255), for: .widget)
+//
+//    }
+//}
+
+//#Preview(as: .accessoryCircular) {
+//    TramComplication()
+//} timeline: {
+//    ComplicationEntry(date: Date(), departure: Departure(time: Date()))
+//    ComplicationEntry(date: Date().addingTimeInterval(3600), departure: Departure(time: Date().addingTimeInterval(3600)))
+//}
+//
+//#Preview(as: .accessoryRectangular) {
+//    TramComplication()
+//} timeline: {
+//    ComplicationEntry(date: Date(), departure: Departure(time: Date()))
+//}
 
 #Preview(as: .accessoryCorner) {
     TramComplication()
 } timeline: {
-    ComplicationEntry(date: Date(), departure: Departure(time: Date()))
+//    ComplicationEntry(date: Date(), departure: Departure(time: Date()))
+    ComplicationEntry(date: Date(), departures: [Departure(time: Date())])
+
 }
 
 @main
